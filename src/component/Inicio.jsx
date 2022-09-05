70d0bf5922ceed9d0763f6a886971b3adaa9be3f
matalala <smitesotra@gmail.com>
2022-09-05 06:53:19 -0300

    update-inicio

--- src/component/Inicio.jsx
+++ src/component/Inicio.jsx
@@ -4,7 +4,7 @@ import imguno from '../resource/img/img-programando-1.png'
 import imgdos from '../resource/img/img-programando-2.png'
 
 const Contenuno=styled.div`
-margin: 10px 10px 10px 15%;
+margin: 10px 10px 10px 10%;
     display: flex;
     align-items: center;
     justify-content: space-around;
@@ -12,17 +12,29 @@ margin: 10px 10px 10px 15%;
     
     `
 const Img=styled.img`
+width: 100%;
+max-width: 400px;
 background-color: rgba(34, 48, 38, 0.49);
 border-radius: 20%;
 padding: 10%;
 `
 const Contendos=styled.div`
-margin-left: 15%;
+margin: 10px 10px 10px 10%;
     display: flex;
     align-items: center;
     justify-content: space-around;
     flex-wrap: nowrap;`
 export default function Inicio(){
+    const fecha= new Date()
+    const nacimiento=new Date('08/04/1995')
+       const dif={
+        mes:(fecha.getMonth()+1)-(nacimiento.getMonth()+1),
+        dia:(fecha.getDate()-nacimiento.getDate()),
+        año:fecha.getFullYear()-nacimiento.getFullYear()
+    }
+    if(dif.mes<0||dif.dia<0){
+        dif.año=dif.año-1
+    }
     return(
         <div>
           <div>
@@ -45,8 +57,9 @@ export default function Inicio(){
                     <Img src={imgdos} alt="" />
                 </div>
                 <div>
-                <h1>HOLA!</h1>
-                    
+                <h1>SOBRE MI!&#128512;</h1>
+                <h3>tengo {dif.año} años&#x1F468;</h3>
+                <h3>soy un apacionado por la programacion &#128187;</h3>
                 </div>
             </Contendos>
 
